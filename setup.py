e02b9a57c3c9a3b32166530bff7f22f83c4dd5ca
bilalakhtar <imBilalx>
2024-03-03 19:33:51 -0500

    changes

---
+++ setup.py
@@ -0,0 +1,31 @@
+from typing import List
+from setuptools import find_packages, setup
+
+HYPHEN_E_DOT = '-e .'
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Read requirements from file
+    :param file_path:
+    :return: List of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+        return requirements
+
+
+setup(
+    name='KN-MLProject',
+    version='0.0.1',
+    author='Bilal Akhtar',
+    author_email='bilalakhtar268@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
